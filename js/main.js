177a55f56804dba801af2f3a3980b09ce026af3c
hayoondevil <hayoondevil@gmail.com>
2021-10-01 16:12:56 +0900

    12th

--- js/main.js
+++ js/main.js
@@ -20,3 +20,26 @@ searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    // 배지 숨기기
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });    
+  } else {
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }  
+}, 300));
+// throttle = lodash 제공 , 300 = 0.3초 => 즉 일정시간 동안 1번씩 실행 되도록 제한
+// _.throttle(함수, 시간)
+
