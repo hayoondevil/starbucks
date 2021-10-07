c8dee79883574407f43e2c434a93c894a0b0f1e1
hayoondevil <hayoondevil@gmail.com>
2021-10-08 06:21:00 +0900

    final

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ searchInputEl.addEventListener('blur', function() {
 });
 
 
-// 배지 관련
+// 배지, 버튼 관련
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function() {
@@ -34,16 +34,33 @@ window.addEventListener('scroll', _.throttle(function() {
       opacity: 0,
       display: 'none'
     });    
+    // 버튼 보이기!
+    gsap.to('#to-top', .2, {
+      x: 0
+    });
   } else {
     // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    // 버튼 숨기기!
+    gsap.to('#to-top', .2, {
+      x: 100
+    });
   }  
 }, 300));
 // throttle = lodash 제공 , 300 = 0.3초 => 즉 일정시간 동안 1번씩 실행 되도록 제한
 // _.throttle(함수, 시간)
+
+
+// 최상단 이동 버튼
+const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0,    
+  });
+});
 
 
 // 순차적으로 이미지 출력
@@ -56,9 +73,8 @@ fadeEls.forEach(function(fadeEl, index) {
 });
 
 
-// Swiper
-// new Swiper(선택자, 옵션)
-new Swiper('.notice-line .swiper-container', {
+// Swiper (자동넘김)
+new Swiper('.notice-line .swiper-container', {   // new Swiper(선택자, 옵션)
   direction: 'vertical',
   autoplay: true,
   loop: true  
@@ -68,9 +84,9 @@ new Swiper('.promotion .swiper-container', {
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
-  // autoplay: {
-  //   delay: 5000
-  // },
+  autoplay: {
+    delay: 4500
+  },
   pagination: {
     el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
     clickable: true // 사용자의 페이지 번호 요소 제어(클릭가능)
@@ -93,7 +109,7 @@ new Swiper('.awards .swiper-container', {
 })
 
 
-
+// 프로모션 토글 처리
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -115,9 +131,8 @@ function random(min, max) {
 }
 
 // floating1,2,3 애니메이션
-function floatingObject(selector, delay, size) {
-  // gsap.to(요소, 시간, 옵션);
-  gsap.to(selector, random(1.5, 2.5), {
+function floatingObject(selector, delay, size) {  
+  gsap.to(selector, random(1.5, 2.5), {   // gsap.to(요소, 시간, 옵션);
     y: size,
     repeat: -1,
     yoyo: true,
@@ -130,7 +145,7 @@ floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
 
 
-// ScrollMagic
+// ScrollMagic (스크롤 위치에 대한 이벤트)
 const spyEls = document.querySelectorAll('section.scroll-spy')
 spyEls.forEach(function (spyEl) {
   // new ScrollMagic.Scene({}).setClassToggle().addTo();를 개행 처리
@@ -143,3 +158,7 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
+
+// 년도 변경 함수
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
