6d72c259fd38bb3e40ea3522b7533dae06f6202f
hayoondevil <hayoondevil@gmail.com>
2021-10-07 14:17:00 +0900

    26th

---
+++ js/youtube.js
@@ -0,0 +1,26 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+function onYouTubeIframeAPIReady() {
+  // <div id="player"></div>
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+    // youtube.com/watch?v=An6LvWQuj_8
+    playerVars: {
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록(loop와 쌍)
+    },
+    events:{
+      onReady: function (event) {
+        event.target.mute() // 음소거(playerVars에서는 제공 안함)
+      }
+    }
+  });
+}
